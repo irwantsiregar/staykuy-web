e4360d5279257f8b09890d54288d2fed1572d66a
Irwanto Siregar <irwanto.regar30@gmail.com>
2025-03-20 10:31:25 +0700

    feat: create pages and integrate to api hotel, city and search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
 import { Inter } from "next/font/google";
 import { Button } from "@heroui/react";
 import PageHead from "@/components/commons/PageHead";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <PageHead title="Home" />
-      <Button color="primary">Button</Button>
+
+      <Button color="primary" onPress={() => router.push("/hotel")}>
+        To Page Hotel
+      </Button>
     </main>
   );
 }
